054d96ac172361c5c802cce30fdbcbe22dde2b9c
Peter Ho <peter.ho@rozettatechnology.com>
2024-11-11 15:44:12 +1100

    feat(nix): add `RTA043`

--- flake.nix
+++ flake.nix
@@ -124,6 +124,12 @@
             modules = [ ./systems/darwin/MBP14-M1.nix ];
           }
         );
+        RTA043 = nix-darwin.lib.darwinSystem (
+          getSystemConfiguration "aarch64-darwin"
+          // {
+            modules = [ ./systems/darwin/RTA043.nix ];
+          }
+        );
       };
 
       homeConfigurations = {
