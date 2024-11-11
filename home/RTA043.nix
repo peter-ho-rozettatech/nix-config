d3f680de3a6b734fe13196cf992925d22284824b
Peter Ho <peter.ho@rozettatechnology.com>
2024-11-11 15:44:12 +1100

    feat(nix): add `RTA043`

---
+++ home/RTA043.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    ./darwin.nix
+  ];
+
+  home = {
+    file = {
+      "iCloud".source = config.lib.file.mkOutOfStoreSymlink (
+        config.home.homeDirectory + "/Library/Mobile Documents/com~apple~CloudDocs"
+      );
+      ".config/git/.gitconfig".source = config.lib.meta.mkDotfilesSymlink "git/.config/git/.gitconfig";
+      ".aws/amazonq/mcp.json".source = config.lib.meta.mkDotfilesSymlink "aws/.aws/amazonq/mcp.json";
+    };
+    sessionVariables = {
+      COPILOT_MODEL = "gpt-4.1";
+      SCRATCH_PATH = "~/iCloud/Documents";
+    };
+    packages = with pkgs; [
+      awscli2
+      terraform
+      # terragrunt
+    ];
+  };
+  programs.opencode.settings = {
+    mcp = {
+      atlassian.enabled = lib.mkForce true;
+      terraform.enabled = lib.mkForce true;
+    };
+  };
+}
