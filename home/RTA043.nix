106633b51d1d80c476eb88f8fa10fb287949c36c
Peter Ho <peter.ho@rozettatechnology.com>
2024-11-11 15:44:12 +1100

    feat(nix): add `RTA043`

---
+++ home/RTA043.nix
@@ -0,0 +1,13 @@
+{ config, ... }:
+{
+  imports = [
+    ./darwin.nix
+  ];
+
+  home.file = {
+    "iCloud".source = config.lib.file.mkOutOfStoreSymlink (
+      config.home.homeDirectory + "/Library/Mobile Documents/com~apple~CloudDocs"
+    );
+    ".config/git/.gitconfig".source = config.lib.meta.mkDotfilesSymlink "git/.config/git/.gitconfig";
+  };
+}
