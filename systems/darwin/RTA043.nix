0ae6e5ad48d06f0294f6cdc62a35e1a8fad42882
Peter Ho <peter.ho@rozettatechnology.com>
2024-11-11 15:44:12 +1100

    feat(nix): add `RTA043`

---
+++ systems/darwin/RTA043.nix
@@ -0,0 +1,66 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  imports = [ ./base.nix ];
+
+  user = lib.mkForce "peterho";
+
+  networking.hostName = "RTA043";
+
+  homebrew = {
+    taps = [
+      "ariga/tap"
+      "localstack/tap"
+    ];
+    brews = [
+      "ariga/tap/atlas"
+      "dnsmasq"
+      "localstack/tap/localstack-cli"
+    ];
+    casks = [
+      # "airbuddy"
+      "aws-vpn-client"
+      "betterdisplay"
+      "claude"
+      "cursorsense"
+      "dbeaver-community"
+      "docker-desktop"
+      "figma"
+      # "jordanbaird-ice"
+      # "kubetail"
+      # "microsoft-auto-update"
+      # "microsoft-teams"
+      # "openvpn-connect"
+      "pgadmin4"
+      "postman"
+      "slack"
+      "stats"
+      "steermouse"
+      "tailscale-app"
+      "thaw"
+      # "visual-studio-code"
+    ];
+  };
+
+  system.defaults.dock = {
+    persistent-apps = lib.mkForce [
+      "/System/Applications/Apps.app"
+      "/System/Cryptexes/App/System/Applications/Safari.app"
+      "/System/Applications/Mail.app"
+      "/System/Applications/Calendar.app"
+      "/System/Applications/Notes.app"
+      "/Applications/Slack.app"
+      "/Applications/Google Chrome.app"
+      "/Applications/Firefox.app"
+      "/Applications/Floorp.app"
+      "/Applications/Ghostty.app"
+      "/Applications/Figma.app"
+    ];
+    persistent-others = lib.mkForce [
+      "${config.homePath}/Downloads"
+    ];
+  };
+}
