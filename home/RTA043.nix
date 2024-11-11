0e683ee3186742ccc533ca642a072800324b23d4
Peter Ho <peter.ho@rozettatechnology.com>
2024-11-11 15:44:12 +1100

    feat(nix): add `RTA043`

---
+++ home/RTA043.nix
@@ -0,0 +1,19 @@
+{ config, ... }:
+{
+  imports = [
+    ./darwin.nix
+  ];
+
+  home = {
+    file = {
+      "iCloud".source = config.lib.file.mkOutOfStoreSymlink (
+        config.home.homeDirectory + "/Library/Mobile Documents/com~apple~CloudDocs"
+      );
+      ".config/git/.gitconfig".source = config.lib.meta.mkDotfilesSymlink "git/.config/git/.gitconfig";
+    };
+    sessionVariables = {
+      COPILOT_MODEL = "claude-sonnet-4";
+      SCRATCH_PATH = "~/iCloud/Documents";
+    };
+  };
+}
