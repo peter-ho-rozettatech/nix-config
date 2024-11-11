5d33882b423a7e17ca0918c9d12acf2cbd16944f
Peter Ho <peter.ho@rozettatechnology.com>
2024-11-11 15:44:12 +1100

    feat(nix): add `RTA043`

---
+++ systems/darwin/RTA043.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  lib,
+  ...
+}:
+{
+  imports = [ ./base.nix ];
+
+  user = lib.mkForce "peterho";
+
+  networking.hostName = "RTA043";
+
+  homebrew = {
+    brews = [
+      "ariga/tap/atlas"
+    ];
+    casks = [
+      "airbuddy"
+      "aws-vpn-client"
+      "betterdisplay"
+      "cursorsense"
+      "datagrip"
+      "dbeaver-community"
+      "docker-desktop"
+      "figma"
+      "headlamp"
+      "jordanbaird-ice"
+      "kiro"
+      "microsoft-auto-update"
+      "microsoft-teams"
+      "openvpn-connect"
+      "pgadmin4"
+      "postman"
+      "slack"
+      "stats"
+      "steermouse"
+      "visual-studio-code"
+    ];
+  };
+
+  system.defaults.dock = {
+    persistent-apps = lib.mkForce [
+      "/System/Applications/Launchpad.app"
+      "/System/Cryptexes/App/System/Applications/Safari.app"
+      "/System/Applications/Mail.app"
+      "/System/Applications/Calendar.app"
+      "/System/Applications/Notes.app"
+      "/Applications/Slack.app"
+      "/Applications/Google Chrome.app"
+      "/Applications/Firefox.app"
+      "/Applications/Floorp.app"
+      "/Applications/Ghostty.app"
+      "/Applications/Figma.app"
+    ];
+    persistent-others = lib.mkForce [
+      "${config.homePath}/Downloads"
+    ];
+  };
+}
