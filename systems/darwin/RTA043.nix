a523c32d74b14f9951e6eae15b80c0d9a11f421c
Peter Ho <peter.ho@rozettatechnology.com>
2024-11-14 11:21:26 +1100

    feat(darwin): add default `persistent-apps` for `RTA043`

--- systems/darwin/RTA043.nix
+++ systems/darwin/RTA043.nix
@@ -1,4 +1,8 @@
-{ lib, ... }:
+{
+  config,
+  lib,
+  ...
+}:
 {
   imports = [ ./base.nix ];
 
@@ -15,4 +19,23 @@
     "postman"
     "slack"
   ];
+
+  system.defaults.dock = {
+    persistent-apps = lib.mkForce [
+      "/System/Applications/Launchpad.app"
+      "/System/Cryptexes/App/System/Applications/Safari.app"
+      "/System/Applications/Mail.app"
+      "/System/Applications/Calendar.app"
+      "/System/Applications/Notes.app"
+      "/Applications/Slack.app"
+      "/Applications/Google Chrome.app"
+      "/Applications/Firefox.app"
+      "/Applications/Floorp.app"
+      "/Applications/WezTerm.app"
+      "/Applications/Figma.app"
+    ];
+    persistent-others = lib.mkForce [
+      "${config.homePath}/Downloads"
+    ];
+  };
 }
