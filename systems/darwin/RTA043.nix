f70c92748f473500d7b374440e86696c1ced4a32
Peter Ho <peter.ho@rozettatechnology.com>
2024-11-21 11:25:40 +1100

    feat(homebrew): add casks

--- systems/darwin/RTA043.nix
+++ systems/darwin/RTA043.nix
@@ -5,4 +5,16 @@
   user = lib.mkForce "peterho";
 
   networking.hostName = "RTA043";
+
+  homebrew.casks = [
+    "airbuddy"
+    "betterdisplay"
+    "docker"
+    "figma"
+    "jordanbaird-ice"
+    "pgadmin4"
+    "postman"
+    "slack"
+    "stats"
+  ];
 }
