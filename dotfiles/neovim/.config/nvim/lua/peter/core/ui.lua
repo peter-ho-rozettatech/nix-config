0c3b32554a40cd67f4324f3d9526e6e39bf20ef1
Peter Tri Ho <mail@petertriho.com>
2026-07-13 20:04:49 +1000

    fix(nvim): use pager for list_cmd

--- dotfiles/neovim/.config/nvim/lua/peter/core/ui.lua
+++ dotfiles/neovim/.config/nvim/lua/peter/core/ui.lua
@@ -5,7 +5,10 @@ local ui2 = require("vim._core.ui2")
 ui2.enable({
     enable = true,
     msg = {
-        targets = "msg",
+        targets = {
+            default = "msg",
+            list_cmd = "pager",
+        },
         cmd = {
             height = 0.3,
         },
