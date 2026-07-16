53f7ac9632d370310e41e2222e34c34f4382277f
Peter Tri Ho <mail@petertriho.com>
2026-07-16 22:45:17 +1000

    fix(nix): update numtide binary cache source

--- flake.nix
+++ flake.nix
@@ -10,13 +10,13 @@
       "https://niri.cachix.org"
       "https://nix-community.cachix.org"
       "https://vicinae.cachix.org"
-      "https://numtide.cachix.org"
+      "https://cache.numtide.com"
     ];
     extra-trusted-public-keys = [
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     ];
   };
 
